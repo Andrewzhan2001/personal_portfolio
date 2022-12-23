3169f6d6ddf3fa4e084472c893b46239925bede4
Tianyi Zhan <hpyzx666666@gmail.com>
2022-12-23 17:03:03 -0500

    update css to scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import {About, Footer, Header, Skills, Testimonial, Work} from './container' //用brace就是自己指定的export, 不用就是default export
 import { Navbar } from './components';
 import './App.scss'
