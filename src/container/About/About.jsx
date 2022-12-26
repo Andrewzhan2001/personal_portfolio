34654281df17c76feabea14cf7b0e19e1a88e3ec
Tianyi Zhan <hpyzx666666@gmail.com>
2022-12-25 23:54:12 -0500

    add animaiton wrapper to each section

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import { motion } from 'framer-motion'
 import { urlFor, client } from '../../client';
-import {AppWrap} from '../../Wrapper'
+import { AppWrap, MotionWrap } from '../../Wrapper';
 import './About.scss'
 
 
@@ -40,4 +40,8 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
